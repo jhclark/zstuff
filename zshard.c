0bf2c8a5cced89488d5ec995ea8e3c963f9f3b20
Jonathan Clark <jon.h.clark@gmail.com>
2012-09-06 10:27:13 -0400

    Fix compile warnings and a few bugs

--- zshard.c
+++ zshard.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 2 // required for popen
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- zsplit.c
+++ zsplit.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 2 // required for popen
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,79 +18,68 @@
 #define MAX_FILENAME_LEN 1024
 
 void showusage() {
-  fprintf(stderr, "usage: zsplit out_prefix < in\n");
+  fprintf(stderr, "usage: zsplit out_prefix max_lines < in\n");
   exit(1);
 }
 
 int main(int argc, char** argv) {
-  if(argc != 2)
+  if (argc != 3)
     showusage();
 
   // TODO: Better arg parsing
   char* out_prefix = argv[1];
-
-  if(colNum < 0)
-    showusage();
+  int max_lines = atoi(argv[2]);
 
 #ifdef USE_ZLIB
-  gzFile out;
+  gzFile out = NULL;
 #else
-  FILE* out;
+  FILE* out = NULL;
 #endif
-
-  char filename[MAX_FILENAME_LEN];
-    sprintf(filename, "%s%06d.gz", prefix, i);
-#ifdef USE_ZLIB
-    shards[i] = gzopen(filename, "wb");
-#else
-    char cmd[MAX_FILENAME_LEN];
-    sprintf(cmd, "gzip > %s", filename);
-    shards[i] = popen(cmd, "w");
-#endif
-    if(shards[i] == NULL) {
-      fprintf(stderr, "Could not open file/pipe for writing: %s", filename);
-      exit(1);
-    }
-  }
 
   char str[MAX_LINE_LEN];
+  int line_idx = 0;
+  int file_idx = 0;
   while(fgets(str, MAX_LINE_LEN, stdin)) {
-    int n = strlen(str);
 
-    char* colBegin = str;
-    char* colEnd = strstr(str, delim);
-    if(colEnd == NULL)
-      colEnd = str + n - 1; // -1 to hack off newline
-    
-    for(int i=1; i <= colNum; ++i) {
-      colBegin = colEnd += delimLen;
-      colEnd = strstr(colBegin, delim);
-      if(colEnd == NULL)
-	colEnd = str + n - 1;
+    // open a new file
+    if (out == NULL || line_idx == max_lines) {
+      if (out != NULL) {
+        file_idx++;
+#ifdef USE_ZLIB
+        gzclose(out);
+#else
+        pclose(out);
+#endif        
+      }
+      char filename[MAX_FILENAME_LEN];
+      sprintf(filename, "%s%06d.gz", out_prefix, file_idx);
+      
+#ifdef USE_ZLIB
+      out = gzopen(filename, "wb");
+#else
+      char cmd[MAX_FILENAME_LEN];
+      sprintf(cmd, "gzip > %s", filename);
+      out = popen(cmd, "w");
+#endif
+      if (out == NULL) {
+        fprintf(stderr, "Could not open file/pipe for writing: %s", filename);
+        exit(1);
+      }
     }
 
-    // now we have the column -- hash it
-    unsigned hashCode = 0;
-    char* lastPos = MIN(colEnd, colBegin + maxCharsToHash);
-    for(char* c = colBegin; c != lastPos; ++c) {
-      hashCode = hashCode * 31 + *c;
-    }
-
-    unsigned shard = hashCode % shardCount;
 #ifdef USE_ZLIB
-    gzputs(shards[shard], str);
+    gzputs(out, str);
 #else
-    fputs(str, shards[shard]);
+    fputs(str, out);
 #endif
+    line_idx++;
   }
 
-  for(int i=0; i<shardCount; ++i) {
 #ifdef USE_ZLIB
-    gzclose(shards[i]);
+  gzclose(out);
 #else
-    pclose(shards[i]);
+  pclose(out);
 #endif
-  }
 
   return 0;
 }
